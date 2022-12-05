22251505ddbddff86213a59c04b5d4b01d6f27ae
orangepi <xsro@foxmail.com>
2022-12-05 13:38:31 +0000

    wip

--- webd.sh
+++ webd.sh
@@ -1,8 +1,8 @@
 systemctl stop systemd-resolved
 sh /home/orangepi/www/clash-iptables.sh
-tinyproxy -c /home/orangepi/www/tinyproxy.conf
+#tinyproxy -c /home/orangepi/www/tinyproxy.conf
 #calibre-server --port=8082 '/home/orangepi/CalibreLibrary' 
-nginx
+#nginx
 
 #source /srv/calibreweb/bin/activate
 /srv/calibreweb/bin/cps 
